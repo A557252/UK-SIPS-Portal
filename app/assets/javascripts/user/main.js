465c448c2080fbdc673cec282a86ac783927c123
[a557252]sandeepkumargupta <sandeep-kumar.gupta@atos.net>
2016-08-23 17:30:53 +0100

    added opbeat

--- app/assets/javascripts/user/main.js
+++ app/assets/javascripts/user/main.js
@@ -5,5 +5,12 @@
 define(['angular', './routes', './services'], function(angular) {
   'use strict';
 
-  return angular.module('yourprefix.user', ['ngCookies', 'ngRoute', 'user.routes', 'user.services']);
+  return angular.module('yourprefix.user', ['ngCookies', 'ngRoute', 'user.routes', 'user.services','ngOpbeat'])
+  .config(function ($opbeatProvider) {
+        $opbeatProvider.config({
+            orgId: '28ce255deee04b7ca965e50a11dcf87b',
+            appId: 'b6b16e205d'
+        })
 });
+
+           
